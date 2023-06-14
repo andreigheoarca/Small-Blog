a4781e39a840403530182ad124704c64500560c5
andreigheoarca <andreigheoarca@gmail.com>
2023-06-14 16:28:51 +0300

    Images fix

--- src/data.js
+++ src/data.js
@@ -2,19 +2,19 @@ const data = [
   {
     id: 1,
     title: "Untitled UI 1",
-    img: "./src/assets/imgs/original-6a6fd58b3eab8edcdb35c5f97d4cfdef.webp",
+    img: "imgs/original-6a6fd58b3eab8edcdb35c5f97d4cfdef.webp",
     contentCreator: "Jordan Hughes®",
   },
   {
     id: 2,
     title: "Untitled UI 2",
-    img: "src/assets/imgs/original-6f754fa0c2efcb92387221c1148ae9c6.webp",
+    img: "imgs/original-6f754fa0c2efcb92387221c1148ae9c6.webp",
     contentCreator: "Jordan Hughes®",
   },
   {
     id: 3,
     title: "Untitled UI 3",
-    img: "src/assets/imgs/original-7db3c2dbaf10c0b08e2620b9acfcf7bc.webp",
+    img: "imgs/original-7db3c2dbaf10c0b08e2620b9acfcf7bc.webp",
     contentCreator: "Jordan Hughes®",
   },
 ];
